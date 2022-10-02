02b582cf338201ffb1ee2e597b1410cff2b21afc
Houtarchat <admin@houtarchat.ml>
2022-10-02 16:04:54 +0800

    captcha

---
+++ captcha.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/base64"
+	"encoding/json"
+)
+
+func getCaptcha(timestamp string) string {
+	url := "https://webservice.forclass.net/Account/GetRegisterCaptcha?stamp=" + timestamp
+	resp, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	return base64.StdEncoding.EncodeToString(body)
+}
+
+func checkCaptcha(captcha string, timestamp string) bool {
+	url := "https://webservice.forclass.net/Account/ValidateRegisterCode?stamp=" + timestamp + "&code=" + captcha
+	resp, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	// json parse body
+	var data map[string]interface{}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return false
+	}
+	retcode := data["retcode"].(float64)
+
+	if retcode == 0 {
+		return true
+	} else {
+		return false
+	}
+}
