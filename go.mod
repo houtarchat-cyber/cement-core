b1ed5d9cb013efcce02d635604e20998318b80a7
Houtarchat <admin@houtarchat.ml>
2022-10-04 18:58:23 +0800

    增加了Clash代理

--- go.mod
+++ go.mod
@@ -7,4 +7,31 @@ require (
 	gopkg.in/yaml.v2 v2.4.0
 )
 
-require golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+require (
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/insomniacslk/dhcp v0.0.0-20220822114210-de18a9d48e84 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/miekg/dns v1.1.50 // indirect
+	github.com/oschwald/geoip2-golang v1.8.0 // indirect
+	github.com/oschwald/maxminddb-golang v1.10.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/u-root/uio v0.0.0-20210528114334-82958018845c // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20220824171710-5757bc0c5503 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20220822230855-b0a4917ee28c // indirect
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/Dreamacro/clash v1.11.8
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
---
+++ proxy.go
@@ -0,0 +1,95 @@
+package cement
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Dreamacro/clash/component/mmdb"
+	"github.com/Dreamacro/clash/constant"
+	"github.com/Dreamacro/clash/hub/executor"
+	"github.com/Dreamacro/clash/log"
+)
+
+func GetClashConfig(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return body
+}
+
+func Proxy(conf []byte) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	constant.SetHomeDir(currentDir)
+
+	err = InitMMDB()
+	if err != nil {
+		panic(err)
+	}
+	cfg, err := executor.ParseWithBytes(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	executor.ApplyConfig(cfg, true)
+
+	// wait for signal (keep process alive)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+}
+
+func InitMMDB() error {
+	if _, err := os.Stat(constant.Path.MMDB()); os.IsNotExist(err) {
+		log.Infoln("Can't find MMDB, start download")
+		if err := DownloadMMDB(constant.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't download MMDB: %s", err.Error())
+		}
+	}
+
+	if !mmdb.Verify() {
+		log.Warnln("MMDB invalid, remove and download")
+		if err := os.Remove(constant.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't remove invalid MMDB: %s", err.Error())
+		}
+
+		if err := DownloadMMDB(constant.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't download MMDB: %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+func DownloadMMDB(path string) (err error) {
+	resp, err := http.Get("https://cdn.jsdelivr.net/gh/Dreamacro/maxmind-geoip@release/Country.mmdb")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, resp.Body)
+
+	return err
+}
